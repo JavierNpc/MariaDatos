71e2f5ce0ad4f8fe5f6ced38c3af58e9c4385024
JavierNpc <javiermacedacastro@gmail.com>
2024-12-11 13:37:02 +0100

    mod

--- AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/PathVariable.java
+++ AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/PathVariable.java
@@ -3,14 +3,13 @@ package com.example.springboot_web.Controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class PathVariable {
     private String tipo;
     private Object direccion;
--- AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/userController.java
+++ AccesoADatos_Maria/Actividad_1/springboot_web/src/main/java/com/example/springboot_web/Controllers/userController.java
@@ -1,13 +1,11 @@
 package com.example.springboot_web.Controllers;
 
 
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot_web.Models.User;
 
